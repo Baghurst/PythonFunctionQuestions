ba3f4509013817c562254e76dd921bf25de594ee
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-22 14:42:04 +0100

    Add files via upload

---
+++ FunctionExercise.py
@@ -0,0 +1,28 @@
+#1
+print("NUM 1")
+
+def prefix(word):
+    added = ('un'+word)
+    return added
+theinput = input("Enter a word: ")
+print(prefix(theinput))
+    
+ #2
+print("NUM 2")
+
+def prefix(word):
+    added = (word+'s')
+    return added
+theinput = input("Enter a word: ")
+print(prefix(theinput))
+        
+#3
+print("NUM 3")
+
+import math
+def radius(pi):
+    lenghtOfRadius = math.pi
+    return lenghtOfCirc
+theinput = input("Enter a number: ")
+print(lenghtOfRadius())
+
