e37a1f6b297acd737038643869549482b35134f5
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-09-22 14:08:28 +0100

    Add files via upload

---
+++ FunctionsExercises.py
@@ -0,0 +1,11 @@
+#Function to calculate lenght of line
+#using pythagoras theorem
+
+import math
+def calculateLineLenght(a,b):
+ #   a = 3
+  #  b = 4
+    lenghtOfSide = math.sqrt(a**2 +b**2)
+    return lenghtOfSide
+print(calculateLineLenght(3,4))
+
